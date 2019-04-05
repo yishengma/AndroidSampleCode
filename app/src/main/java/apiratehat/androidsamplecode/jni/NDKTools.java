f0a89525a019f3db43fcd0bb2eba949e63439a69
e <13104862372@163.com>
2019-04-05 22:03:57 +0800

    NDK 解决gradle版本问题

--- app/src/main/java/apiratehat/androidsamplecode/jni/NDKTools.java
+++ app/src/main/java/apiratehat/androidsamplecode/jni/NDKTools.java
@@ -1,11 +1,18 @@
 package apiratehat.androidsamplecode.jni;
 
 /**
+ *
  * Created by PirateHat on 2019/4/3.
  */
 
 public class NDKTools {
+    static {
+        System.loadLibrary("ndkdemotest-jni");
+    }
+
 
     public static native String getStringFromNDK();
 
 }
+
+
